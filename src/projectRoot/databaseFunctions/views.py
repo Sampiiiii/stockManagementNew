7e09452b66ffd530b38dbb3dda406589ea18f0d1
Sampiiiii <34628661+Sampiiiii@users.noreply.github.com>
2019-12-07 18:58:44 +0000

    fixed untracked files 2

--- src/projectRoot/databaseFunctions/views.py
+++ src/projectRoot/databaseFunctions/views.py
@@ -1,20 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import ModelFormWithFileField
 
-def upload_file(request):
+def upload(request):
     if request.method == 'POST':
-        form = ModelFormWithFileField(request.POST, request.FILES)
-        if form.is_valid():
-            # file is saved
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = ModelFormWithFileField()
-    return render(request, 'upload.html', {'form': form})
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return render(request, 'upload.html')
