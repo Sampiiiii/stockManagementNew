6b3642cfb7d726ac4429d40d45894e5628ab1fb9
Sampiiiii <sampii@protonmail.com>
2020-01-28 10:47:17 +0000

    Added Models for graphing Module and added package

--- src/projectRoot/graphingModule/models.py
+++ src/projectRoot/graphingModule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from databaseFunctions.models import product
 
 # Create your models here.
+class Facilities(models.Model):
+    FacilityChoices = [
+        ('aaa', 'null')
+    ]
+    FacilityLocation = models.CharField(max_length=3, choices=FacilityChoices, default='aaa')
+
+class productHistory(models.Model):
+    class Meta:
+        unique_together = (('productDate', 'productID'),)
+    productDate = models.DateField(auto_now=False, auto_now_add=True)
+    productID = models.ForeignKey(product, on_delete=models.CASCADE)
+    FacilityID = models.ForeignKey(Facilities, on_delete=models.CASCADE)
+    OpeningBalance = models.IntegerField()
+    ForecastedUsage = models.IntegerField(null=True)
+
+
--- src/projectRoot/project/urls.py
+++ src/projectRoot/project/urls.py
@@ -1,24 +1,27 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from databaseFunctions import views_database_functions
-from core import views_core
+from databaseFunctions import views_database_functions as vdf
+from core import views_core as vc
 from graphingModule import views_graphing_module
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views_core.Home.as_view(), name='home'),
+    path('', vc.Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('documents/', views_database_functions.document_list, name='document_list'),
-    path('documents/upload', views_database_functions.upload_document, name='upload_document'),
-    path('documents/<int:pk>/', views_database_functions.delete_document, name='delete_document'),
-    path('documents/process/<int:pk>/', views_database_functions.process_document, name='process_document'),
-    path('products/', views_database_functions.product_list, name='product_list'),
-    path('products/delete/<int:pk>', views_database_functions.delete_product, name='delete_product'),
-    path('products/delete/all', views_database_functions.delete_all_products, name='delete_all_products'),
-    path('products/modify/<int:pk>/',views_database_functions.modify_product, name='modify_product'),
-    path('products/add', views_database_functions.product_add, name='product_add'),
+
+    path('documents/', vdf.document_list, name='document_list'),
+    path('documents/upload', vdf.upload_document, name='upload_document'),
+    path('documents/<int:pk>/', vdf.delete_document, name='delete_document'),
+    path('documents/process/<int:pk>/', vdf.process_document, name='process_document'),
+
+    path('products/', vdf.product_list, name='product_list'),
+    path('products/delete/<int:pk>', vdf.delete_product, name='delete_product'),
+    path('products/delete/all', vdf.delete_all_products, name='delete_all_products'),
+    path('products/modify/<int:pk>/',vdf.modify_product, name='modify_product'),
+    path('products/add', vdf.product_add, name='product_add'),
+    
 ]
 
 if settings.DEBUG:
