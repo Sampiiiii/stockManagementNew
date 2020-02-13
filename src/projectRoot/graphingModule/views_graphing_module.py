bbfce772308a78575c6a0f69fe81cb98fc4705c3
Sampiiiii <34628661+Sampiiiii@users.noreply.github.com>
2020-02-13 22:10:17 +0000

    Added Function Templates

--- src/projectRoot/graphingModule/views_graphing_module.py
+++ src/projectRoot/graphingModule/views_graphing_module.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-from .forms import facilityForm, productHistoryForm;
-from .models import facilities, productHistory;
+from .forms import facilityForm, productHistoryForm
+from .models import facilities, productHistory
 
-# Create your views here.
+# Product History Views
 def productHistoryList(request):
     productHistories = productHistoryForm.objects.all()
     return render(request, 'document')
@@ -14,4 +14,29 @@ def addProductHistory(request):
     form = productHistoryForm(request.POST)
     if form.is_valid():
         form.save()
-        
+
+def modifyProductHistory(request, pk):
+    pass
+
+def deleteProductHistory(request, pk):
+    pass
+
+def deleteAllProductHistories(request):
+    pass
+
+# Facility Views
+def facilityList(request):
+    pass
+
+def addFacility(request):
+    pass
+
+def modifyFacility(request, pk):
+    pass
+
+def deleteFacility(request, pk):
+    pass
+
+def deleteAllFacilities(request):
+    pass
+
